c754da2700baf6a21c53ff5c029a27dde37f8750
hualy <784975128@qq.com>
2017-02-18 12:54:42 +0800

    Initial commit

---
+++ hello.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/2/18.
+ */
+require('./world.js');
+require('style-loader!css-loader!./style.css');
+
+function hello(str){
+    alert(str);
+}
+
+hello('hello world!!!wd');
---
+++ world.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/2/18.
+ */
+function world(){
+    return {}
+}
